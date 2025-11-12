29f4d9596fa23b80d41a0a3e81186b460a50e8dc
Miksa <miksa.pribus@aalto.fi>
2025-11-12 18:13:12 +0200

    adding missing files

---
+++ Assets/Scripts/AudioManger.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if(instance != null){
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start(){
+        Play("Music");
+    }
+
+    public void Play(string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null){
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
