1f5bfc39a166265eb9d60c6fcf6315dcd7d137df
Miksa <miksa.pribus@aalto.fi>
2025-11-14 10:34:55 +0200

    Added extra objective

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public bool taken = false;
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
-            SceneController.instance.NextLevel();
+            taken = true;
+            Goal[] goals = transform.parent.GetComponentsInChildren<Goal>();
+            bool isAll = true;
+            foreach(Goal plate in goals){
+                if (!plate.taken) isAll = false;
+            }
+            if(isAll)
+                SceneController.instance.NextLevel();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            taken = false;
         }
     }
 }
